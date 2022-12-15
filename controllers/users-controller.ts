a9119378ac259bc6dab91ba5e4bcc1bb60204e8a
Clam119 <Christopher.lam119@gmail.com>
2022-12-15 16:45:14 +0000

    Created the postNewUser controller function.

--- controllers/users-controller.ts
+++ controllers/users-controller.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { fetchAllUsers } from "../../models/users-model";
+import { fetchAllUsers, createNewUser } from "../models/users-model";
 
 export async function getAllUsers(req: NextApiRequest, res: NextApiResponse) {
     const allUsersData = await fetchAllUsers();
@@ -9,5 +9,17 @@ export async function getAllUsers(req: NextApiRequest, res: NextApiResponse) {
     }
     catch(err) {
         console.log(err);
+    }
+}
+
+export async function postNewUser(req: NextApiRequest, res: NextApiResponse) {
+    const { username, name, avatar_url } = req.body;
+    const postedUser = await createNewUser(username, name, avatar_url);
+    try {
+        res.status(201)
+        .send(postedUser);
+    }
+    catch(err: any) {
+        console.log(err, '<< Error in users controller.')
     }
 }
