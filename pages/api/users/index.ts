3058c056b5b6538471f839a40053377ce5d12ab2
Clam119 <Christopher.lam119@gmail.com>
2022-12-15 16:40:57 +0000

    Remove the try/catch block since the controller & model handles the errors.

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,16 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getAllUsers } from "../../../controllers/users/getAllUsers";
+import { getAllUsers, postNewUser } from "../../../controllers/users-controller";
 
 export default async function handleUsers(req: NextApiRequest, res: NextApiResponse) {
     switch(req.method) {
         case 'GET':
-            try {
-                return  getAllUsers(req, res);
-            }
-            catch(err) {
-                console.log(err)
-            }
-            break;
+            return getAllUsers(req, res);
+        case 'POST': 
+            return postNewUser(req, res);
         default: 
             res.status(500).json({msg: 'Bad Request.'})
     }
