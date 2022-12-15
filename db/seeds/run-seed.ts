bbbb3e53b22a22926b1c7db09986f7727fef56a7
Clam119 <Christopher.lam119@gmail.com>
2022-12-15 16:43:30 +0000

    Change the seed to seed both the test and development databases.

--- db/seeds/run-seed.ts
+++ db/seeds/run-seed.ts
@@ -1,10 +1,16 @@
 // Import in the connection to db, seed itself and development data to seed
 import devData from '../data/development-data/index';
+import testData from '../data/test-data';
 import seed from './seed';
 import db from '../connection';
 
-const runSeed = () => {
-  return seed(devData).then(() => db.end());
+const runTestSeed = () => {
+  return seed(testData).then(() => db.end());
 };
 
-runSeed();
+const runDevSeed = () => {
+  return seed(devData).then(() => db.end());
+}
+
+runTestSeed();
+runDevSeed();
