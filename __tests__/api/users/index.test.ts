a92dc9b90b56f8a9a5bbe145861715af938256dc
Clam119 <Christopher.lam119@gmail.com>
2022-12-15 16:44:40 +0000

    Added a beforeAll & afterAll command so that it seeds before each test and ends connection. In addition, added the successful & failed HTTP request tests.

--- __tests__/api/users/index.test.ts
+++ __tests__/api/users/index.test.ts
@@ -2,8 +2,12 @@ import handleUsers from '../../../pages/api/users/index';
 import db from '../../../db/connection';
 import { createMocks } from 'node-mocks-http';
 import User from '../../../lib/usersInterface';
+import seed from '../../../db/seeds/seed';
+import testData from '../../../db/data/test-data';
 
-afterAll(() => db.end())
+beforeAll(() => seed(testData));
+afterAll(() => db.end());
+
 
 describe('GET /api/users', () => {
 
@@ -59,9 +63,9 @@ describe('GET /api/users', () => {
 describe('POST /api/users', () => {
     it('Should return a status code of 201 if a successful POST request is made.', async() => {
         // Create Mock Req & Res to /api/users
-        const newUser: User = { username: 'clam119', name: 'Christopher Lam', avatar_url: 'https://i.ibb.co/M2nHQSC/me-and-best-mates.jpg'}
+        const newUser: User = { username: 'NAL1998', name: 'Nhat Anh Le', avatar_url: 'https://scontent.fman4-2.fna.fbcdn.net/v/t1.18169-9/12243282_405218356351547_3411609076197569911_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=174925&_nc_ohc=NCFTrzMEyREAX-7w8_f&tn=GUMDvshL0XzfMHPY&_nc_ht=scontent.fman4-2.fna&oh=00_AfBOLIKq5HOqVG7Lgxdmb3i3GnK5vNLIkXGyKAl3wBz9GA&oe=63C18241'}
         const { req, res } = createMocks({
-            method: 'GET',
+            method: 'POST',
             body:  newUser
         })
         // Wait for the handleUsers Function to resolve with the designated POST request & check code
@@ -73,8 +77,8 @@ describe('POST /api/users', () => {
     it('Should return an object containing the new user that was successfully posted.', async() => {
         const newUser: User = { username: 'clam119', name: 'Christopher Lam', avatar_url: 'https://i.ibb.co/M2nHQSC/me-and-best-mates.jpg'}
         const { req, res } = createMocks({
-            method: 'GET',
-            body:  newUser
+            method: 'POST',
+            body: newUser
         })
         await handleUsers(req, res);
         const responseData = res._getData();
@@ -84,4 +88,40 @@ describe('POST /api/users', () => {
             avatar_url: 'https://i.ibb.co/M2nHQSC/me-and-best-mates.jpg'
         })
     })
+
+    it('Should return a status code of 400 if any of the parameters aren\'t strings.', async() => {
+        const invalidUser = { username: 123, name: 'Invalid Username Haha', avatar_url: 'true' };
+        const { req, res } = createMocks({
+            method: 'POST',
+            body: invalidUser
+        })
+        
+        try {
+            await handleUsers(req, res);
+        }
+        catch(err: any) {
+            const { status, msg } = err;
+            expect(status).toBe(400)
+            expect(msg).toBe('Invalid Fields Data');
+        }
+    })
+
+    it('Should return a status code of 400 with a custom message if username already exists.', async() => {
+        const newUser: User = { username: 'clam119', name: 'Christopher Lam', avatar_url: 'https://i.ibb.co/M2nHQSC/me-and-best-mates.jpg'}
+        const { req, res } = createMocks({
+            method: 'POST',
+            body: newUser
+        })
+
+        try {
+            await handleUsers(req, res);
+        }
+        catch(err: any) {
+            const { status, msg } = err;
+            expect(status).toBe(400)
+            expect(msg).toBe('Username Already Exists')
+        }
+    })
 })
+
+
