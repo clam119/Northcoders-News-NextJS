37936fad0eb174180a9277d7bc1965ea76d63571
Clam119 <Christopher.lam119@gmail.com>
2022-12-16 22:07:53 +0000

    initial commit - Add fetchAllTopics and createNewTopic model functions.

---
+++ models/topics-model.ts
@@ -0,0 +1,31 @@
+import db from "../db/connection";
+import Topic from "../lib/topicsInterface";
+import Error from "../lib/errorInterface";
+
+export async function fetchAllTopics() {
+    const queryData = await db.query('SELECT * FROM topics');
+    const { rows: allTopicsData } = queryData;
+    return allTopicsData;
+}
+
+export async function createNewTopic(slug: string, description: string) {
+    const topicsQuery = await db.query('SELECT * FROM topics;')
+    const existingTopics = await topicsQuery.rows.map((topic: Topic): string => topic.slug)
+
+    if (existingTopics.includes(slug)) {
+        return Promise.reject({ status: 409, msg: 'That topic already exists.'})
+    }
+
+    if (slug === undefined || description === undefined) {
+        return Promise.reject({ status: 404, msg: 'Missing Required Fields' })
+    }
+
+    if (typeof slug !== 'string' || typeof description !== 'string') {
+        return Promise.reject({ status: 400, msg: 'Invalid Fields Data' })
+    }
+
+    const createTopic = await db.query(`INSERT INTO topics(slug, description) VALUES ($1, $2) RETURNING *;`, [slug, description])
+    const queryData = await createTopic;
+    const { rows: [newTopicData] } = queryData;
+    return newTopicData;
+}
