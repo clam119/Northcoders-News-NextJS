7c43329d6ecfd0738600a77fb2d1935dcb9a09d9
Clam119 <Christopher.lam119@gmail.com>
2022-12-16 18:33:42 +0000

    Add the fetchSingleUser model function.

--- models/users-model.ts
+++ models/users-model.ts
@@ -6,6 +6,29 @@ export interface Error {
     msg: string
 }
 
+export async function fetchSingleUser(username: string): Promise <User | Error> {
+    const usernameQuery = await db.query('SELECT * FROM users;')
+    const validUsernames = await usernameQuery.rows.map((user: User): string => user.username)
+    
+    //Checks to see if the requested username is the correct data type.
+    if(typeof username !=='string'){
+        return Promise.reject({ status: 400, msg: "Invalid Fields Data" })
+    }
+    
+    //Check the database for queried username, returns promise rejection if not found.
+    if(!validUsernames.includes(username)) {
+        return Promise.reject({ status: 404, msg: "Username Not Found" });
+    }
+
+    
+    //If the passed in username is valid - query and return user data
+    const response = await db.query(`SELECT * FROM users WHERE username = $1`, [username])
+    const responseData = await response;
+    const { rows: [singleUserData] } = responseData;
+
+    return singleUserData;
+} 
+
 export async function fetchAllUsers(): Promise<User[]> {
     const queryData  = await db.query('SELECT * FROM users;');
     const { rows: allUsersData } = queryData;
