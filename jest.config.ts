ef2fe3548a1bbb4ad0368e1c9cc20a54ae518ef7
Clam119 <Christopher.lam119@gmail.com>
2022-12-18 17:49:01 +0000

    Fixed Jest being unable to resolve modules by adding the moduleNameMapper and rootDir so that it knows where to look for absolute imports.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
 // module.exports = {
 //     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-//     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
 //   };
 
 import type {Config} from 'jest';
@@ -9,6 +8,15 @@ const config: Config = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  rootDir: "./",
+  moduleNameMapper: {
+    '^@db/(.*)': '<rootDir>/db/$1',
+    '^@lib/(.*)': '<rootDir>/lib/$1',
+    '^@pages/(.*)': '<rootDir>/pages/$1',
+    '^@controllers/(.*)': '<rootDir>/controllers/$1',
+    '^@models/(.*)': '<rootDir>/models/$1',
+    '^@public/(.*)': '<rootDir>/public/$1',
+  }
 };
 
 export default config;
