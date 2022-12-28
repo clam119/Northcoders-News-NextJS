50109d7e3b9bc32a99af9d587832048230f79cc3
Clam119 <Christopher.lam119@gmail.com>
2022-12-28 14:12:01 +0000

    Changed comment_id to be optional because when you make a post request it will automatically generate a comment_id and changed the created_at to its correct typing of a number.

--- lib/commentsInterface.ts
+++ lib/commentsInterface.ts
@@ -1,8 +1,8 @@
 export default interface Comment {
-    comment_id: string,
+    comment_id?: string,
     body: string,
     article_id: number,
     author: string,
     votes: number,
-    created_at: string
+    created_at: number
 }
