ff6dc755445418346772d70d558759cbff6ff8f9
Clam119 <Christopher.lam119@gmail.com>
2022-12-31 14:39:48 +0000

    Added the article-comments rewrites.

--- next.config.js
+++ next.config.js
@@ -32,8 +32,19 @@ const nextConfig = {
       {
         source: '/docs/articles/delete-articles-by-id',
         destination: '/articles/delete-articles-by-id.html'
-      }
+      },
 
+      //Article-Comments Rewrites
+      {
+        source: '/docs/articles-comments/get-articles-comments',
+        destination: '/articles-comments/get-articles-comments.html'
+      },
+
+      {
+        source: '/docs/articles-comments/post-articles-comments',
+        destination: '/articles-comments/post-articles-comments.html'
+      },
+      
     ]
   }
 }
