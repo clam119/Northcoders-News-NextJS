f53602b13d0aeba00d54dc2cf5fab8d5c0c27587
Clam119 <Christopher.lam119@gmail.com>
2022-12-31 13:13:44 +0000

    Added rewrites for docs/articles.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,40 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/docs',
+        destination: '/index.html',
+      },
+      // Articles Rewrites
+      {
+        source: '/docs/articles/get-articles',
+        destination: '/articles/get-articles.html',
+      },
+
+      {
+        source: '/docs/articles/post-articles',
+        destination: '/articles/post-articles.html'
+      },
+
+      {
+        source: '/docs/articles/get-articles-by-id',
+        destination: '/articles/get-articles-by-id.html'
+      },
+
+      {
+        source: '/docs/articles/patch-articles-by-id',
+        destination: '/articles/patch-articles-by-id.html'
+      },
+
+      {
+        source: '/docs/articles/delete-articles-by-id',
+        destination: '/articles/delete-articles-by-id.html'
+      }
+
+    ]
+  }
 }
 
 module.exports = nextConfig
