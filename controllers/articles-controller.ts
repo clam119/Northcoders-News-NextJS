bf88013e52166e20c2b65b64914c8ec8b01a5689
Clam119 <Christopher.lam119@gmail.com>
2023-01-01 18:10:24 +0000

    Add the request queries and try/catch block to complete the getAllArticles Controller Function.

--- controllers/articles-controller.ts
+++ controllers/articles-controller.ts
@@ -9,7 +9,15 @@ import {
 }   from '@models/articles-model'
 
 export async function getAllArticles(req: NextApiRequest, res: NextApiResponse) {
-
+    const { topic, sort_by, order, limit, p } = req.query as { topic: string, sort_by: string, order: string, limit: any, p: any };
+    const allArticlesData = await fetchAllArticles(topic, sort_by, order, limit, p);
+    try {
+        res.status(200)
+        .send(allArticlesData)
+    }
+    catch(err) {
+        console.log(err, '< /api/articles - getAllArticles Controller Function Error.')
+    }
 }
 
 export async function postNewArticle(req: NextApiRequest, res: NextApiResponse) {
